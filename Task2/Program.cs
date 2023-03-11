2bb0c656d45250ac00d68e5fc3691008acf9834f
Valera <Valera@ASUSX556U>
2023-03-11 10:06:24 +0200

    Добавьте файлы проекта.

---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+
+    /*
+    Використовуючи Visual Studio, створіть проект за шаблоном Console Application. 
+    Створіть клас MyList. 
+    Реалізуйте у найпростішому наближенні можливість використання його екземпляра аналогічно екземпляру класу List. 
+    Мінімально необхідний інтерфейс взаємодії з екземпляром повинен включати метод додавання елемента, 
+    індексатор для отримання значення елемента за вказаним індексом і властивість тільки для читання 
+    для отримання загальної кількості елементів.
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
